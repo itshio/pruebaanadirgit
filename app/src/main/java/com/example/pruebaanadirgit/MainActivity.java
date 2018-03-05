e12cbab034ae5a611b12bb459a069f81565d2e7e
darkclif <morpheoosmatrix@gmail.com>
2018-03-05 12:21:46 +0100

    prueba simplemente subida desde clase

--- app/src/main/java/com/example/pruebaanadirgit/MainActivity.java
+++ app/src/main/java/com/example/pruebaanadirgit/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.pruebaanadirgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView suma;
+    int parametro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        suma=(TextView)findViewById(R.id.textosumado);
+
+
+    }
+
+    public void sumar (View view) {
+
+        if(parametro<100){
+            parametro=parametro+1;
+
+            suma.setText(parametro+"");}
+
+
+
     }
 }
